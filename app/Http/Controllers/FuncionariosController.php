e7e4e855ae3f7b967b6d9dc5b0e10a4e904e8e64
Guilherme Cezarini Vieira <cezarini.guilherme2@gmail.com>
2020-06-24 09:53:00 -0300

    adicionando validação na data de demissão

--- app/Http/Controllers/FuncionariosController.php
+++ app/Http/Controllers/FuncionariosController.php
@@ -132,19 +132,22 @@ class FuncionariosController extends Controller
                     continue;
                 }
 
-                if($key == 'dt_adimissao' || ($key == 'dt_adimissao' && $value))
+                if($key == 'dt_adimissao' || ($key == 'dt_demissao' && $value))
                 {
                     $funcionario->$key = $this->parseDate($value);
                     continue;
                 }
-
                 $funcionario->$key = $value;
             }
+
+            if($funcionario->dt_adimissao->gt($funcionario->dt_demissao)) return redirect("/funcionarios/alterar/{$funcionario->id}")->with('message', 'Data de demissão não pode ser menor que a de adimissão.');
+
             $funcionario->save();
 
             return redirect('/funcionarios')->with('message', 'Funcionário atualizado com sucesso.');
 
         }catch(\Exception $e){
+            dd($e);
             return redirect('/funcionarios')->with('message', 'Não foi possivel atualizar o funcionário, tente mais tarde.');
         }
 
